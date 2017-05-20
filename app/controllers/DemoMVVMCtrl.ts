8277d9bff2c8b4c1a49fafaeb3cc2778972d40f6
parag diwan <paragdiwan@gmail.com>
2017-05-20 22:45:27 +0100

    Dummy service implementation in controller

--- app/controllers/DemoMVVMCtrl.ts
+++ app/controllers/DemoMVVMCtrl.ts
@@ -6,6 +6,7 @@
     export interface IMessage {
         displayText: string;
         flag: boolean;
+        serviceResponse: any;
         changeScopeModel(): void;
     }
 
@@ -13,14 +14,15 @@
     export class DemoMVVMCtrl  implements IMessage {
         displayText: string;
         flag: boolean;
-        constructor() {
-            this.displayText = "Welcome to Typescript-";
+        serviceResponse: any;
+        static $inject = ['$http'];
+        constructor(private $http: ng.IHttpService) {
+            this.displayText = "Welcome to Typescript";
             this.flag = true; 
         }
 
         changeScopeModel() {
             this.flag = !this.flag;
-
             if(this.flag) {
                 this.displayText = "You are loving TS."
             }
@@ -28,9 +30,19 @@
                 this.displayText = "Welcome to Typescript";
             }
         }
-        
+
+        getDataFromService() {
+            this.$http.get("http://date.jsontest.com/")
+                .then((response) => {
+                    try {
+                        this.serviceResponse = response.data;
+                    }
+                    catch (exception) {
+                        throw exception;
+                    }
+                });
+        }
     }
-    
 }
 
 angular.module('app',[])
